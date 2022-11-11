0fc2ef8418831a6c89de177ac63ed8201980f4cd
Philip Lages <psilvaelages@gmail.com>
2022-11-11 16:04:29 -0300

    feat: create connection to database

---
+++ src/models/products.model.js
@@ -0,0 +1,12 @@
+const connection = require("./db/connection")
+
+const getAllProducts = async () => {
+  const [ result ] = await connection.execute(
+    'SELECT * FROM StoreManager.products',
+  );
+  return result;
+};
+
+module.exports = {
+  getAllProducts
+}
