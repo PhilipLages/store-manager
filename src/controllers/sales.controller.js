b3672f727290f8afd0c052059b6512bd984cb3d0
Philip Lages <psilvaelages@gmail.com>
2022-11-14 14:43:23 -0300

    feat: create controllers layer for sales, as well as createNewSales function

---
+++ src/controllers/sales.controller.js
@@ -0,0 +1,14 @@
+const salesService = require('../services/sales.service');
+const httpStatus = require('../utils/httpStatus');
+
+const createNewSales = async (req, res) => {
+  const sales= req.body;
+
+  const result = await salesService.createNewSales(sales);
+
+  return res.status(httpStatus.CREATED).json(result);
+};
+
+module.exports = {
+  createNewSales,
+};
